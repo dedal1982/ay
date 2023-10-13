64243aee2aaad0ec1bc16ae4e22f24a9a795c270
AlDm <d44.a1@yandex.ru>
2023-10-13 08:03:59 +0300

    1.3

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2.7,
-    spaceBetween: 32,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 2.7,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 var swiper2 = new Swiper(".team__slider", {
-    slidesPerView: 2.5,
-    spaceBetween: 32,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  slidesPerView: 2.5,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
+  },
 });
 var swiper3 = new Swiper(".feedback__slider", {
-    slidesPerView: 1,
-    spaceBetween: 16,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  slidesPerView: 1,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
     },
+  },
 });
 
 var swiper4 = new Swiper(".situation__slider", {
-    slidesPerView: 3.2,
-    spaceBetween: 32,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3.2,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 var swiper5 = new Swiper(".lawyer__slider", {
-    slidesPerView: 1.2,
-    spaceBetween: 32,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 1.2,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 var swiper6 = new Swiper(".services__slider-1", {
-    slidesPerView: 2.5,
-    spaceBetween: 32,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 2.5,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 var swiper7 = new Swiper(".services__slider-2", {
-    slidesPerView: 2.5,
-    spaceBetween: 32,
-    navigation: {
-        nextEl: ".swiper-button-next2",
-        prevEl: ".swiper-button-prev2",
-    },
+  slidesPerView: 2.5,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
 });
