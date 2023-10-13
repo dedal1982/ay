dc2b7ce9671f976768acef9de7e5c9c1e902e074
AlDm <d44.a1@yandex.ru>
2023-10-13 03:18:15 +0300

    1.0

---
+++ script.js
@@ -0,0 +1,57 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2.7,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper2 = new Swiper(".team__slider", {
+    slidesPerView: 2.5,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper3 = new Swiper(".feedback__slider", {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper4 = new Swiper(".situation__slider", {
+    slidesPerView: 3.2,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper5 = new Swiper(".lawyer__slider", {
+    slidesPerView: 1.2,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper6 = new Swiper(".services__slider-1", {
+    slidesPerView: 2.5,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+var swiper7 = new Swiper(".services__slider-2", {
+    slidesPerView: 2.5,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+});
