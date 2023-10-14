e9c8733eca1ba1ae2bfbf9e84748b0e0c25a5e20
AlDm <d44.a1@yandex.ru>
2023-10-14 17:57:41 +0300

    ggg

--- script.js
+++ script.js
@@ -1,65 +1,3 @@
-const burgerBtn = document.querySelector(".burbtn");
-const burgerMenu = document.querySelector(".burger__menu");
-const lockBody = document.querySelector(".body-page");
-burgerBtn.addEventListener("click", () => {
-  console.log(11111);
-  burgerMenu.classList.toggle("burger__menu_open");
-  lockBody.classList.toggle("lock");
-});
-
-//перебор по ссылкам мобильное меню
-const linkMobileMenu = document.querySelectorAll(".burger__link");
-
-for (link of linkMobileMenu) {
-  link.addEventListener("click", function () {
-    burgerMenu.classList.remove("burger__menu_open");
-    lockBody.classList.remove("lock");
-  });
-}
-//десктоп бургер
-const burgerDesctop = document.querySelector(".burger__desctop");
-const burgerDescBtn = document.querySelector(".desb");
-const burgerDescBtnClose = document.querySelector(".desctop-logo-img");
-
-burgerDescBtn.addEventListener("click", () => {
-  burgerDesctop.classList.add("burger__desctop_open");
-  lockBody.classList.add("lock");
-});
-burgerDescBtnClose.addEventListener("click", () => {
-  burgerDesctop.classList.remove("burger__desctop_open");
-  lockBody.classList.remove("lock");
-});
-//функция переключения табов
-const makeCodeUniversal = (
-  tabItemsQuery,
-  formItemsQuery,
-  tabClassName = "active"
-) => {
-  const tabItems = Array.from(document.querySelectorAll(tabItemsQuery));
-  const formItems = Array.from(document.querySelectorAll(formItemsQuery));
-
-  const clearActiveTabs = (element) => {
-    element.find((item) => item.classList.remove(tabClassName));
-  };
-
-  const setActiveTab = (element, index) => {
-    element[index].classList.add(tabClassName);
-  };
-
-  const chekTab = (item, index) => {
-    item.addEventListener("click", () => {
-      clearActiveTabs(tabItems);
-      clearActiveTabs(formItems);
-
-      setActiveTab(tabItems, index);
-      setActiveTab(formItems, index);
-    });
-  };
-  tabItems.forEach(chekTab);
-};
-// /*табы / секция Наши реализованные проекты*/
-makeCodeUniversal(".burger__title", ".burger__links-item");
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2.7,
   spaceBetween: 32,
@@ -198,3 +136,86 @@ var swiper7 = new Swiper(".services__slider-2", {
     },
   },
 });
+//--------------------------------------
+const burgerBtn = document.querySelector(".burbtn");
+const burgerMenu = document.querySelector(".burger__menu");
+const lockBody = document.querySelector(".body-page");
+burgerBtn.addEventListener("click", () => {
+  console.log(11111);
+  burgerMenu.classList.toggle("burger__menu_open");
+  lockBody.classList.toggle("lock");
+});
+
+//перебор по ссылкам мобильное меню
+const linkMobileMenu = document.querySelectorAll(".burger__link");
+
+for (link of linkMobileMenu) {
+  link.addEventListener("click", function () {
+    burgerMenu.classList.remove("burger__menu_open");
+    lockBody.classList.remove("lock");
+  });
+}
+
+//десктоп бургер
+const burgerDesctop = document.querySelector(".burger__desctop");
+const burgerDescBtn = document.querySelector(".desb");
+const burgerDescBtnClose = document.querySelector(".desctop-logo-img");
+
+burgerDescBtn.addEventListener("click", () => {
+  burgerDesctop.classList.add("burger__desctop_open");
+  lockBody.classList.add("lock");
+});
+burgerDescBtnClose.addEventListener("click", () => {
+  burgerDesctop.classList.remove("burger__desctop_open");
+  lockBody.classList.remove("lock");
+});
+//перебор ссылок десктоп меню
+const linkDesktopMenu = document.querySelectorAll(".burger__desctop-link");
+
+for (link of linkDesktopMenu) {
+  link.addEventListener("click", function () {
+    burgerDesctop.classList.remove("burger__desctop_open");
+    lockBody.classList.remove("lock");
+  });
+}
+//перебор ссылок десктоп меню
+const linkDesktopMenuServise = document.querySelectorAll(
+  ".burger__services-link"
+);
+for (link of linkDesktopMenuServise) {
+  link.addEventListener("click", function () {
+    burgerDesctop.classList.remove("burger__desctop_open");
+    lockBody.classList.remove("lock");
+  });
+}
+
+//функция переключения табов
+const makeCodeUniversal = (
+  tabItemsQuery,
+  formItemsQuery,
+  tabClassName = "active"
+) => {
+  const tabItems = Array.from(document.querySelectorAll(tabItemsQuery));
+  const formItems = Array.from(document.querySelectorAll(formItemsQuery));
+
+  const clearActiveTabs = (element) => {
+    element.find((item) => item.classList.remove(tabClassName));
+  };
+
+  const setActiveTab = (element, index) => {
+    element[index].classList.add(tabClassName);
+  };
+
+  const chekTab = (item, index) => {
+    item.addEventListener("click", () => {
+      clearActiveTabs(tabItems);
+      clearActiveTabs(formItems);
+
+      setActiveTab(tabItems, index);
+      setActiveTab(formItems, index);
+    });
+  };
+  tabItems.forEach(chekTab);
+};
+// /*табы / секция Наши реализованные проекты*/
+makeCodeUniversal(".burger__title", ".burger__links-item");
